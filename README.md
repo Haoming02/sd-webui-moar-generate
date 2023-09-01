f7579acde89374b79e5427305dae47be210f1166
Haoming <hmstudy02@gmail.com>
2023-09-01 23:00:38 +0800

    3rd mode
    Optimized the button size as well~

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 ﻿# SD Webui Moar Generate
-This is an Extension for the [Automatic1111 Webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui), 
-which adds another **Generate** button, 
+This is an Extension for the [Automatic1111 Webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui) which adds another **Generate** button, 
 for when you have too many Extensions active but don't wanna scroll all the way up every single time... 
 
-By default, it adds the button to the bottom of the page.
-You can also edit the `.js` file to change the setting and move the button under **Result** instead.
+## Setting
+You can right click the `.js` file, click **Edit**, then modify the following line to select between different options:
+
+- `static option = x`
+  > Replace **x** with:
+
+  - **0**: Add the button at the bottom of the page
+  - **1**: Add the button under the generated result
+  - **2**: Add the button at the fixed bottom right corner
+
--- javascript/moar_gen.js
+++ javascript/moar_gen.js
@@ -1,16 +1,15 @@
-﻿class Butt {
+﻿class AndAnotherOne {
 
     static Modes = ['txt', 'img']
 
     // === SETTING ===
-    static option = 0 // 0: Bottom ; 1: Result
+    static option = 0 // 0: Bottom ; 1: Result ; 2: Floating
     // === SETTING ===
 
-    static main() {
-
+    static butt() {
         this.Modes.forEach((mode) => {
             const generate = document.getElementById(mode + '2img_generate')
-            const tab = (this.option === 0) ? document.getElementById('tab_' + mode + '2img') : document.getElementById(mode + '2img_results')
+            const tab = document.getElementById('tab_' + mode + '2img')
 
             const btn = generate.cloneNode()
 
@@ -19,13 +18,73 @@
             })
 
             btn.innerHTML = 'Generate'
-            btn.style.width = '100%'
+
+            btn.style.position = 'relative'
+            btn.style.minHeight = '2.5em'
+            btn.style.width = '60%'
+            btn.style.left = '20%'
+            btn.style.margin = '1em 0px 0px 0px'
 
             tab.appendChild(btn)
+        })
+    }
+
+    static result() {
+        this.Modes.forEach((mode) => {
+            const generate = document.getElementById(mode + '2img_generate')
+            const box = document.getElementById(mode + '2img_results')
+
+            const btn = generate.cloneNode()
+
+            btn.addEventListener('click', () => {
+                generate.dispatchEvent(new Event('click'))
+            })
+
+            btn.innerHTML = 'Generate'
+
+            btn.style.position = 'relative'
+            btn.style.minHeight = '2.5em'
+            btn.style.width = '80%'
+            btn.style.left = '10%'
+
+            box.appendChild(btn)
+        })
+    }
+
+    static fixed() {
+        this.Modes.forEach((mode) => {
+            const generate = document.getElementById(mode + '2img_generate')
+            const tab = document.getElementById('tab_' + mode + '2img')
+
+            const btn = generate.cloneNode()
+
+            btn.addEventListener('click', () => {
+                generate.dispatchEvent(new Event('click'))
+            })
+
+            btn.innerHTML = 'Generate'
+
+            btn.style.position = 'fixed'
+            btn.style.minHeight = '4em'
+            btn.style.width = '8em'
+            btn.style.right = '3em'
+            btn.style.bottom = '6em'
+
+            tab.append(btn)
         })
     }
 }
 
 onUiLoaded(async () => {
-    Butt.main()
+    switch (AndAnotherOne.option) {
+        default:
+            AndAnotherOne.butt()
+            break;
+        case 1:
+            AndAnotherOne.result()
+            break;
+        case 2:
+            AndAnotherOne.fixed()
+            break;
+    }
 })
