675cdc652a680b1a42084307e524391562abf647
Haoming <hmstudy02@gmail.com>
2023-09-01 18:14:51 +0800

    init
    yeet

---
+++ README.md
@@ -0,0 +1,6 @@
+﻿# SD Webui Bottom Generate
+This is an Extension for the [Automatic1111 Webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui), 
+which adds another **Generate** button at the bottom of the page, 
+for when you have too many Extensions active but don't wanna scroll all the way up every time... 
+
+Yep, that's all... 💀
---
+++ javascript/bottom_gen.js
@@ -0,0 +1,26 @@
+﻿class Butt {
+
+    static Modes = ['txt', 'img']
+
+    static main() {
+        this.Modes.forEach((mode) => {
+            const generate = document.getElementById(mode + '2img_generate')
+            const tab = document.getElementById('tab_' + mode + '2img')
+
+            const btn = generate.cloneNode()
+
+            btn.addEventListener('click', () => {
+                generate.dispatchEvent(new Event('click'))
+            })
+
+            btn.innerHTML = 'Generate'
+            btn.style.width = '100%'
+
+            tab.appendChild(btn)
+        })
+    }
+}
+
+onUiLoaded(async () => {
+    Butt.main()
+})
