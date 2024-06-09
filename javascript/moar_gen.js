814db21166003fcad2e84e90be2aabe9d637f858
bluelovers <codelovers@users.sourceforge.net>
2024-06-09 23:30:42 +0800

    refactor: code splitting

--- javascript/moar_gen.js
+++ javascript/moar_gen.js
@@ -1,17 +1,24 @@
 ﻿class AndAnotherOne {
 
+    /**
+     * @param mode string
+     */
+    static clone(mode) {
+        const generate = document.getElementById(mode + '_generate')
+        const btn = generate.cloneNode()
+        btn.innerHTML = 'Generate'
+        btn.id = ''
+
+        btn.addEventListener('click', () => {
+            generate.dispatchEvent(new Event('click'))
+        })
+
+        return btn
+    }
+
     static butt() {
         ['txt2img', 'img2img', 'extras'].forEach((mode) => {
-            const generate = document.getElementById(mode + '_generate')
-            const tab = document.getElementById('tab_' + mode)
-
-            const btn = generate.cloneNode()
-
-            btn.addEventListener('click', () => {
-                generate.dispatchEvent(new Event('click'))
-            })
-
-            btn.innerHTML = 'Generate'
+            const btn = AndAnotherOne.clone(mode)
 
             btn.style.position = 'relative'
             btn.style.minHeight = '2.5em'
@@ -19,58 +26,44 @@
             btn.style.left = '20%'
             btn.style.margin = '1em 0px 0px 0px'
 
+            const tab = document.getElementById('tab_' + mode)
             tab.appendChild(btn)
         })
     }
 
     static result() {
         ['txt2img', 'img2img', 'extras'].forEach((mode) => {
-            const generate = document.getElementById(mode + '_generate')
-            const box = document.getElementById(mode + '_results')
-
-            const btn = generate.cloneNode()
-
-            btn.addEventListener('click', () => {
-                generate.dispatchEvent(new Event('click'))
-            })
-
-            btn.innerHTML = 'Generate'
+            const btn = AndAnotherOne.clone(mode)
 
             btn.style.position = 'relative'
             btn.style.minHeight = '2.5em'
             btn.style.width = '80%'
             btn.style.left = '10%'
 
+            const box = document.getElementById(mode + '_results')
             box.appendChild(btn)
         })
     }
 
     static fixed() {
         ['txt2img', 'img2img', 'extras'].forEach((mode) => {
-            const generate = document.getElementById(mode + '_generate')
-            const tab = document.getElementById('tab_' + mode)
-
-            const btn = generate.cloneNode()
-
-            btn.addEventListener('click', () => {
-                generate.dispatchEvent(new Event('click'))
-            })
-
-            btn.innerHTML = 'Generate'
+            const btn = AndAnotherOne.clone(mode)
 
             btn.style.position = 'fixed'
             btn.style.minHeight = '4em'
             btn.style.width = '8em'
             btn.style.right = '3em'
             btn.style.bottom = '6em'
+            btn.style.zIndex = '5000'
 
+            const tab = document.getElementById('tab_' + mode)
             tab.append(btn)
         })
     }
 
 }
 
-onUiLoaded(async () => {
+(typeof onUiLoadedReady !== 'undefined' ? onUiLoadedReady : onUiLoaded)(async () => {
     // 0: Bottom ; 1: Result ; 2: Floating
     const config = gradioApp().getElementById('setting_moar_generate').querySelectorAll('label')
     let option = -1;
